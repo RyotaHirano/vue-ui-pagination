ac16a58e3921ac58ffd1fcb409a854c57521a87f
Ryota Hirano <hirano@tsumikiinc.com>
2017-03-26 11:41:05 +0900

    wip pagination debug

--- src/components/carouselPager.vue
+++ src/components/carouselPager.vue
@@ -18,9 +18,21 @@
       returnStoreImages: function() {
         if(this.$store.state.images.length > 5) {
           if(this.$store.state.images.length > this.$store.state.currentIdx + 5) {
+            // ロード時
+            console.log('a');
             return this.$store.state.images.slice(this.$store.state.currentIdx, this.$store.state.currentIdx + 5).concat()
+          } else if (this.$store.state.images.length - 1 === this.$store.state.currentIdx) {
+            // 最後
+            console.log('d');
+            return this.$store.state.images.slice(this.$store.state.images.length - 6, this.$store.state.images.length - 1).concat()
+          } else if (this.$store.state.images.length - 2 <= this.$store.state.currentIdx) {
+            // 最後から2つ前〜１つ前
+            console.log('c');
+            return this.$store.state.images.slice(this.$store.state.images.length - 6, this.$store.state.images.length - 1).concat()
           } else {
-            return this.$store.state.images.slice(this.$store.state.images.length - 5, this.$store.state.images.length).concat()
+            // 通常
+            console.log('b');
+            return this.$store.state.images.slice(this.$store.state.images.length - 6, this.$store.state.images.length - 1).concat()
           }
         } else {
           return this.$store.state.images.concat()
